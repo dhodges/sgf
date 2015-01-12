0299509170d7cd48888f4d4534d400ff6788f700
david hodges <david.hodges@lonelyplanet.com.au>
2015-01-12 16:00:49 +1100

    added simple Stack

---
+++ parse/stack_test.go
@@ -0,0 +1,35 @@
+package parse
+
+import "testing"
+
+func TestStack(t *testing.T) {
+	stack := new(Stack)
+
+	stack.Push("H")
+	stack.Push("R")
+	stack.Push("Puffenstuff")
+
+	if stack.Len() != 3 {
+		t.Error("stack count is wrong")
+	}
+
+	item := stack.Peek()
+	if item != "Puffenstuff" {
+		t.Errorf("stack is wrong, expected 'Puffenstuff' but found %q", item)
+	}
+
+	item = stack.Pop()
+	if item != "Puffenstuff" {
+		t.Errorf("stack is wrong, expected 'Puffenstuff' but found %q", item)
+	}
+
+	item = stack.Pop()
+	if item != "R" {
+		t.Errorf("stack is wrong, expected 'R' but found %q", item)
+	}
+
+	item = stack.Pop()
+	if item != "H" {
+		t.Errorf("stack is wrong, expected 'H' but found %q", item)
+	}
+}
